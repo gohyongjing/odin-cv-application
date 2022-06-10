9fac409bbb3b2329e5dcc94e26de181a14b706dd
Yong Jing <gohyongjing@gmail.com>
2022-06-10 12:34:46 +0800

    Rewrite FormSection.js to practice hooks

--- src/components/FormSection.js
+++ src/components/FormSection.js
@@ -1,7 +1,7 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 
 import FormPreview from './FormPreview';
-
+/*
 class FormSection extends Component {
     constructor(props) {
         super(props);
@@ -48,5 +48,59 @@ class FormSection extends Component {
         );
     }
 }
+*/
+
+const FormSection = (props) => {
+
+    const [editing, setEditing] = useState(true);
+
+    const initialInputs = {};
+    for (const field of props.fields) {
+        initialInputs[field.id] = {
+            label: field.label,
+            value: ""
+        };
+    }
+    const [inputs, setInputs] = useState(initialInputs);
+
+    const onEdit = (e) => {        
+        const newInputs = {...inputs}
+        newInputs[e.target.id].value = e.target.value;
+        setInputs(newInputs);
+    }
+
+    const onToggle = (e) => {
+        setEditing(!editing);
+    }
+
+    return (
+        <div className="form-section">
+            <h1>{props['section-name']}</h1>
+            {
+                editing
+                    ? props.fields.map((field) => {
+                        return (
+                            <div key={field.id}>
+                                <label htmlFor={field.id}>
+                                    {field.label}
+                                </label>
+                                <input
+                                    id={field.id}
+                                    type={field.type}
+                                    value={inputs[field.id].value}
+                                    onChange={onEdit}
+                                />
+                            </div>
+                        )
+                    })
+                    : <FormPreview fields={inputs} />
+            }
+            <input type='button'
+                value={editing ? 'Submit' : 'Edit'}
+                onClick={onToggle}>
+            </input>
+        </div>
+    );
+};
 
 export default FormSection;
